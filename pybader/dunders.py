c6be32385d9a533c52110f43832b458ef7c6bd34
Adam Kerrign <ak1014@york.ac.uk>
2020-04-16 01:28:19 +0100

    bumped version number

--- pybader/dunders.py
+++ pybader/dunders.py
@@ -9,7 +9,7 @@ from sys import platform
 import os
 
 __name__ = "pybader"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Adam Kerrigan"
 __email__ = "ak1014@york.ac.uk"
 __url__ = "https://github.com/kerrigoon/pybader"
